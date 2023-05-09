a4abd74b3074ddaff6b1e86b0c70bf20872b00bb
BlueAcidLies <noablueice@gmail.com>
2023-05-09 17:21:35 +0200

    Ainhoa a las 17:21

---
+++ ClaseJimenez.java
@@ -0,0 +1,17 @@
+public class ClaseJimenez
+{
+
+    String nombre;
+
+    public ClaseJimenez(String as_nombre)
+    {
+        nombre = as_nombre;   
+    }
+
+    // clase donde se escribe el nombre
+    public void escribirnombre()
+    {
+        System.out.println("Mi nombre es: " + nombre);
+    }
+
+}
