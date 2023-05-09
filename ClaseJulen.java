c744aa69ccfe1b688f69572a99010f0fd93ae4b4
aramburujulen <127227978+aramburujulen@users.noreply.github.com>
2023-05-09 17:01:02 +0200

    Add files via upload
    16:59 Julen Aramburu

---
+++ ClaseJulen.java
@@ -0,0 +1,17 @@
+public class ClaseJulen
+{
+
+    String nombre;
+
+    public ClaseJulen(String as_nombre)
+    {
+        nombre = as_nombre;   
+    }
+
+    // clase donde se escribe el nombre
+    public void escribirnombre()
+    {
+        System.out.println("Mi nombre es: " + nombre);
+    }
+
+}
