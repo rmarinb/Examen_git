5e36cda718e33e1917f21ee6ad55d7e006fdd831
Rodrigo <rodrigoomg6@gmail.com>
2023-05-09 17:37:44 +0200

    Rodrigo 17:37

---
+++ ClaseRodrigo.java
@@ -0,0 +1,17 @@
+public class ClaseRodrigo
+{
+
+    String nombre;
+
+    public ClaseRodrigo(String as_nombre)
+    {
+        nombre = as_nombre;   
+    }
+
+    // clase donde se escribe el nombre
+    public void escribirnombre()
+    {
+        System.out.println("Mi nombre es: " + nombre);
+    }
+
+}
