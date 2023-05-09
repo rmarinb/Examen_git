a07ccae8d47acf8ffe4b0d72d818684c0b160651
Samuel Zudaire Aldama <zudaire.alsam22@zaragoza.salesianos.edu>
2023-05-09 16:56:01 +0200

    Samuel Zudaire 09/05/2023 Añdido Clase Zudaire

---
+++ ClaseZudaire.java
@@ -0,0 +1,12 @@
+public class ClaseZudaire {
+    
+    String nombre;
+
+    public ClaseZudaire(String as_nombre){
+        nombre = as_nombre;
+    }
+
+    public void escribirnombre(){
+        System.out.println("Mi nombre es: " + nombre);
+    }
+}
