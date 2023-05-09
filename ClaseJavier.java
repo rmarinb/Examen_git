176538f9f1c81a2611722e73507b35f9350a7255
javi152001 <olivan.agjav22@zaragoza.salesianos.edu>
2023-05-09 16:36:33 +0200

    16:36 JavierOlivan

---
+++ ClaseJavier.java
@@ -0,0 +1,14 @@
+public class ClaseJavier {
+    String nombre;
+
+    public ClaseJavier(String as_nombre)
+    {
+        nombre = as_nombre;   
+    }
+
+    // clase donde se escribe el nombre
+    public void escribirnombre()
+    {
+        System.out.println("Mi nombre es: " + nombre);
+    }
+}
