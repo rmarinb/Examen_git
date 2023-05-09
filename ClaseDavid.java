101aa6a73d9fd650b95b02f2a3f077a9eeaa7cea
unknown <aznar.ardav20@zaragoza.salesianos.edu>
2023-05-09 17:37:54 +0200

    David Aznar 17:37

---
+++ ClaseDavid.java
@@ -0,0 +1,12 @@
+public class ClaseDavid {
+	String nombre;
+	// Coje el nombre que le da el usuario.
+	public ClaseDavid(String as_nombre) {
+		nombre = as_nombre;
+	}
+	//Escribe el nombre dicho en un print.
+	public void escribirnombre()
+    {
+        System.out.println("Mi nombre es: " + nombre);
+    }
+}
