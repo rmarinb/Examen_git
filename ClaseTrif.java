35d15d350094e0b2cd16529b19f4ac03948c219a
DanielTrif1 <trif.geo22@zaragoza.salesianos.edu>
2023-05-09 16:55:16 +0200

    George Daniel Trif 16:56

---
+++ ClaseTrif.java
@@ -0,0 +1,19 @@
+package entornosDeDesarollo;
+
+public class ClaseTrif
+{
+
+    String nombre;
+
+    public ClaseTrif(String as_nombre)
+    {
+        nombre = as_nombre;   
+    }
+
+    // clase donde se escribe el nombre
+    public void escribirnombre()
+    {
+        System.out.println("Mi nombre es: " + nombre);
+    }
+
+}
